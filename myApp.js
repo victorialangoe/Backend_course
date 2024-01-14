b2b53b681a444ee9cfc5bcfa68a29014bf1ae370
Victoria Ovedie Chruickshank Langø <victorialangoe@icloud.com>
2024-01-14 19:44:06 +0100

    finished 2.2

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
