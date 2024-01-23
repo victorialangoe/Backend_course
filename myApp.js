53ae38c2b25515b33cbd6bc401705ac16ca3be44
Victoria Ovedie Chruickshank Langø <victorialangoe@icloud.com>
2024-01-23 21:45:11 +0100

    finished task 7/12

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let app = express();
 absolutePath = __dirname + "/views/index.html";
 let message = { message: "Hello json" };
 
+app.use((req, res, next) => {
+  let string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
